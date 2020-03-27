4c803fdfb7a51f851823bb352b7422c99f2031c6
Marek Anderson <marek.anderson@arctiq.ca>
2020-03-27 09:32:05 -0400

    fix json file creation

--- createJSON.sh
+++ createJSON.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-jo -p pretty=true type=Service filter="match(\"*swap*\", service.name)" author=icingaadmin comment="Who needs swap" fixed=true start_time=$(date +%s -d "+0 hour") end_time=$(date +%s -d "+1 minute")
+jo -p pretty=true type=Service filter="match(\"*swap*\", service.name)" author=icingaadmin comment="Who needs swap" fixed=true start_time=$(date +%s -d "+0 hour") end_time=$(date +%s -d "+1 minute") > file.json
 
 exit 0
