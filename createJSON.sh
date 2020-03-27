153f0b95914b2dfd09f9b2dbb25194621c8b9075
Marek Anderson <manderson.it@gmail.com>
2020-03-27 09:13:52 -0400

    Create createJSON.sh

---
+++ createJSON.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+jo -p pretty=true type=Service filter="match(\"*swap*\", service.name)" author=icingaadmin comment="Who needs swap" fixed=true start_time=$(date +%s -d "+0 hour") end_time=$(date +%s -d "+1 minute")
+
+exit 0
